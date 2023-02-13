82bb7bda670ff52b615b7778b5f8291564375134
Andres Mendez <fmendez1212@gmail.com>
2023-02-12 21:07:53 -0500

    switch to postgress 🐘

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,21 +1,32 @@
-const mongoose = require('mongoose');
-const mongodbUri = process.env.MONGODB_URI;
+const Sequelize = require("sequelize");
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, dialect } = process.env;
 
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
     console.log("connecting to database");
-    mongoose.connect(mongodbUri, (err) => {
-        if(err) {
-            console.log(err);
-            return callback("error conecting to database",err);
-        }
-        console.log("successfully connected to database");
-        _db = mongoose.connection;
+    _db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+      host: DB_HOST,
+      dialect: "postgres",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
     });
+
+    _db
+      .authenticate()
+      .then(() => {
+        console.log("Connection has been established successfully.");
+      })
+      .catch((err) => {
+        console.error("Unable to connect to the database:", err);
+      });
   },
- 
+
   getDb: function () {
     return _db;
   },
