183f1e2294136a22541d7fefd28431749746396f
Andres Mendez <fmendez1212@gmail.com>
2023-02-19 09:57:56 -0500

    see other player moves

--- server/server.js
+++ server/server.js
@@ -40,10 +40,12 @@ io.on("connection", (socket) => {
   
   socket.on("getBoard", (params) => {
     console.log("a user requested a gameBoard", params);
+    socket.join(params.gameId);
 
     db.getGame(params.gameId, (game,error) => {
        if(!error) {
          console.log("returned game " + params.gameId, game.id);
+         
          socket.emit("updateBoard", game.board);
        } else {
         console.log("error returning game " + params.gameId, error);
@@ -54,23 +56,17 @@ io.on("connection", (socket) => {
     });
   });
 
-  // latency, I think I should just return the move
   socket.on("playerMove", (move) => {
     console.log("a user made a move", move);
     db.updateBoard(move, (updateBoard,error) => {
       if(!error) {
-        socket.emit("updateBoard", updateBoard);
+        io.sockets.in(move.gameId).emit("updateBoard", updateBoard);
       } else {
         socket.emit("updateBoard", {
           message: error
         });
       }
     });
-    
   });
 
-  socket.on("ping", (params) => {
-    io.to(socket.id).emit("pong");
-    console.log("a user send ping", params);
-  });
 });
